6d42cfadb11a92d20f68ea4f021616c7d0ae4354
mfu2002 <Faisalmohammed2002@gmail.com>
2021-02-28 12:17:34 +1100

    Add project files.

---
+++ ScoreBoard/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace ScoreBoard
+{
+    partial class frmScoreboard
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.pnlMiddle = new System.Windows.Forms.Panel();
+            this.pnlBottom = new System.Windows.Forms.Panel();
+            this.pnlTopLeft = new System.Windows.Forms.Panel();
+            this.pnlTopRight = new System.Windows.Forms.Panel();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pnlBottomLeft = new System.Windows.Forms.Panel();
+            this.pnlBottomRight = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.Red;
+            this.pnlTop.Location = new System.Drawing.Point(41, 28);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(100, 20);
+            this.pnlTop.TabIndex = 0;
+            // 
+            // pnlMiddle
+            // 
+            this.pnlMiddle.BackColor = System.Drawing.Color.Red;
+            this.pnlMiddle.Location = new System.Drawing.Point(41, 175);
+            this.pnlMiddle.Name = "pnlMiddle";
+            this.pnlMiddle.Size = new System.Drawing.Size(100, 20);
+            this.pnlMiddle.TabIndex = 1;
+            // 
+            // pnlBottom
+            // 
+            this.pnlBottom.BackColor = System.Drawing.Color.Red;
+            this.pnlBottom.Location = new System.Drawing.Point(41, 322);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Size = new System.Drawing.Size(100, 20);
+            this.pnlBottom.TabIndex = 2;
+            // 
+            // pnlTopLeft
+            // 
+            this.pnlTopLeft.BackColor = System.Drawing.Color.Red;
+            this.pnlTopLeft.Location = new System.Drawing.Point(41, 54);
+            this.pnlTopLeft.Name = "pnlTopLeft";
+            this.pnlTopLeft.Size = new System.Drawing.Size(20, 115);
+            this.pnlTopLeft.TabIndex = 3;
+            // 
+            // pnlTopRight
+            // 
+            this.pnlTopRight.BackColor = System.Drawing.Color.Red;
+            this.pnlTopRight.Location = new System.Drawing.Point(121, 54);
+            this.pnlTopRight.Name = "pnlTopRight";
+            this.pnlTopRight.Size = new System.Drawing.Size(20, 115);
+            this.pnlTopRight.TabIndex = 4;
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Location = new System.Drawing.Point(167, 28);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(70, 70);
+            this.btnSeven.TabIndex = 5;
+            this.btnSeven.Text = "7";
+            this.btnSeven.UseVisualStyleBackColor = true;
+            this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(167, 272);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(70, 70);
+            this.btnOne.TabIndex = 6;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // btnFour
+            // 
+            this.btnFour.Location = new System.Drawing.Point(167, 150);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(70, 70);
+            this.btnFour.TabIndex = 7;
+            this.btnFour.Text = "4";
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // btnFive
+            // 
+            this.btnFive.Location = new System.Drawing.Point(277, 150);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(70, 70);
+            this.btnFive.TabIndex = 10;
+            this.btnFive.Text = "5";
+            this.btnFive.UseVisualStyleBackColor = true;
+            this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Location = new System.Drawing.Point(277, 272);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(70, 70);
+            this.btnTwo.TabIndex = 9;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnEight
+            // 
+            this.btnEight.Location = new System.Drawing.Point(277, 28);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(70, 70);
+            this.btnEight.TabIndex = 8;
+            this.btnEight.Text = "8";
+            this.btnEight.UseVisualStyleBackColor = true;
+            this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+            // 
+            // btnSix
+            // 
+            this.btnSix.Location = new System.Drawing.Point(387, 150);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(70, 70);
+            this.btnSix.TabIndex = 13;
+            this.btnSix.Text = "6";
+            this.btnSix.UseVisualStyleBackColor = true;
+            this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Location = new System.Drawing.Point(387, 272);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(70, 70);
+            this.btnThree.TabIndex = 12;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnNine
+            // 
+            this.btnNine.Location = new System.Drawing.Point(387, 28);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(70, 70);
+            this.btnNine.TabIndex = 11;
+            this.btnNine.Text = "9";
+            this.btnNine.UseVisualStyleBackColor = true;
+            this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(40, 356);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(101, 33);
+            this.btnReset.TabIndex = 14;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // pnlBottomLeft
+            // 
+            this.pnlBottomLeft.BackColor = System.Drawing.Color.Red;
+            this.pnlBottomLeft.Location = new System.Drawing.Point(41, 201);
+            this.pnlBottomLeft.Name = "pnlBottomLeft";
+            this.pnlBottomLeft.Size = new System.Drawing.Size(20, 115);
+            this.pnlBottomLeft.TabIndex = 4;
+            // 
+            // pnlBottomRight
+            // 
+            this.pnlBottomRight.BackColor = System.Drawing.Color.Red;
+            this.pnlBottomRight.Location = new System.Drawing.Point(121, 201);
+            this.pnlBottomRight.Name = "pnlBottomRight";
+            this.pnlBottomRight.Size = new System.Drawing.Size(20, 115);
+            this.pnlBottomRight.TabIndex = 15;
+            // 
+            // frmScoreboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 401);
+            this.Controls.Add(this.pnlBottomRight);
+            this.Controls.Add(this.pnlBottomLeft);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.pnlTopRight);
+            this.Controls.Add(this.pnlTopLeft);
+            this.Controls.Add(this.pnlBottom);
+            this.Controls.Add(this.pnlMiddle);
+            this.Controls.Add(this.pnlTop);
+            this.MinimumSize = new System.Drawing.Size(520, 440);
+            this.Name = "frmScoreboard";
+            this.Text = "Scoreboard";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Panel pnlMiddle;
+        private System.Windows.Forms.Panel pnlBottom;
+        private System.Windows.Forms.Panel pnlTopLeft;
+        private System.Windows.Forms.Panel pnlTopRight;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Panel pnlBottomLeft;
+        private System.Windows.Forms.Panel pnlBottomRight;
+    }
+}
+
---
+++ ScoreBoard/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScoreBoard
+{
+    public partial class frmScoreboard : Form
+    {
+        public frmScoreboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = Color.Red;
+            pnlTop.BackColor = Color.Red;
+            pnlTopRight.BackColor = Color.Red;
+            pnlBottomRight.BackColor = Color.Red;
+            pnlBottom.BackColor = Color.Red;
+            pnlBottomLeft.BackColor = Color.Red;
+            pnlMiddle.BackColor = SystemColors.ControlDark;
+        }
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = SystemColors.ControlDark;
+            pnlTop.BackColor = SystemColors.ControlDark;
+            pnlTopRight.BackColor = Color.Red;
+            pnlBottomRight.BackColor = Color.Red;
+            pnlBottom.BackColor = SystemColors.ControlDark;
+            pnlBottomLeft.BackColor = SystemColors.ControlDark;
+            pnlMiddle.BackColor = SystemColors.ControlDark;
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = SystemColors.ControlDark;
+            pnlTop.BackColor = Color.Red;
+            pnlTopRight.BackColor = Color.Red;
+            pnlBottomRight.BackColor = SystemColors.ControlDark;
+            pnlBottom.BackColor = Color.Red;
+            pnlBottomLeft.BackColor = Color.Red;
+            pnlMiddle.BackColor = Color.Red;
+
+
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = SystemColors.ControlDark;
+            pnlTop.BackColor = Color.Red;
+            pnlTopRight.BackColor = Color.Red;
+            pnlBottomRight.BackColor = Color.Red;
+            pnlBottom.BackColor = Color.Red;
+            pnlBottomLeft.BackColor = SystemColors.ControlDark;
+            pnlMiddle.BackColor = Color.Red;
+        }
+
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = Color.Red;
+            pnlTop.BackColor = SystemColors.ControlDark;
+            pnlTopRight.BackColor = Color.Red;
+            pnlBottomRight.BackColor = Color.Red;
+            pnlBottom.BackColor = SystemColors.ControlDark;
+            pnlBottomLeft.BackColor = SystemColors.ControlDark;
+            pnlMiddle.BackColor = Color.Red;
+        }
+
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = Color.Red;
+            pnlTop.BackColor = Color.Red;
+            pnlTopRight.BackColor = SystemColors.ControlDark;
+            pnlBottomRight.BackColor = Color.Red;
+            pnlBottom.BackColor = Color.Red;
+            pnlBottomLeft.BackColor = SystemColors.ControlDark;
+            pnlMiddle.BackColor = Color.Red;
+        }
+
+        private void btnSix_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = Color.Red;
+            pnlTop.BackColor = Color.Red;
+            pnlTopRight.BackColor = SystemColors.ControlDark;
+            pnlBottomRight.BackColor = Color.Red;
+            pnlBottom.BackColor = Color.Red;
+            pnlBottomLeft.BackColor = Color.Red;
+            pnlMiddle.BackColor = Color.Red;
+        }
+
+        private void btnSeven_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = SystemColors.ControlDark;
+            pnlTop.BackColor = Color.Red;
+            pnlTopRight.BackColor = Color.Red;
+            pnlBottomRight.BackColor = Color.Red;
+            pnlBottom.BackColor = SystemColors.ControlDark;
+            pnlBottomLeft.BackColor = SystemColors.ControlDark;
+            pnlMiddle.BackColor = SystemColors.ControlDark;
+        }
+
+        private void btnEight_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = Color.Red;
+            pnlTop.BackColor = Color.Red;
+            pnlTopRight.BackColor = Color.Red;
+            pnlBottomRight.BackColor = Color.Red;
+            pnlBottom.BackColor = Color.Red;
+            pnlBottomLeft.BackColor = Color.Red;
+            pnlMiddle.BackColor = Color.Red;
+        }
+
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+            pnlTopLeft.BackColor = Color.Red;
+            pnlTop.BackColor = Color.Red;
+            pnlTopRight.BackColor = Color.Red;
+            pnlBottomRight.BackColor = Color.Red;
+            pnlBottom.BackColor = Color.Red;
+            pnlBottomLeft.BackColor = SystemColors.ControlDark;
+            pnlMiddle.BackColor = Color.Red;
+        }
+    }
+}
